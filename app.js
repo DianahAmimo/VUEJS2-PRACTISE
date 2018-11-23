9f01ed743a528fbd6c253db012d0b7c5b7035dd6
Dianne <dianneprinsescah@gmail.com>
2018-11-23 11:46:39 +0300

    Data and methods

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
-  el: '#vue-app',
-  data: {
-    name: 'Dianne'
-  }
-})
+    el: '#vue-app',
+
+    data: {
+        name: 'Dianne',
+        job: 'Softwareentwickler'
+    },
+
+    methods: {
+      greet: function (time) {
+          return 'Guten ' + time + ' '+ this.name;
+      }
+    }
+});
