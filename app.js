184107bebd78e585faa704b74aaeff351d06afc6
Dianne <dianneprinsescah@gmail.com>
2018-11-23 12:06:10 +0300

    Data binding

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ new Vue({
 
     data: {
         name: 'Dianne',
-        job: 'Softwareentwickler'
+        job: 'Softwareentwickler',
+        website: 'https://www.linkedin.com/in/dianah-amimo-4b8036107/',
+        websiteTag: '<a href="https://www.linkedin.com/in/dianah-amimo-4b8036107/">My LinkedIn Profile</a>'
     },
 
     methods: {
